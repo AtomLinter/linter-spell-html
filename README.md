a10207091777bd372882116616e55f8bc2e38072
Tarn Burton <twburton@gmail.com>
2016-07-02 09:45:51 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# linter-spell-html [![Travis-CI Build Status](https://img.shields.io/travis/yitzchak/linter-spell-html/master.svg?label=Linux/OSX%20build)](https://travis-ci.org/yitzchak/linter-spell-html) [![AppVeyor Build Status](https://img.shields.io/appveyor/ci/yitzchak/linter-spell-html/master.svg?label=Windows%20build)](https://ci.appveyor.com/project/yitzchak/linter-spell-html) [![David](https://img.shields.io/david/yitzchak/linter-spell-html.svg)](https://david-dm.org/yitzchak/linter-spell-html)
+
+Provider for Atom [linter-spell](https://atom.io/packages/linter-spell) package
+to enable HTML spell checking.
+
+## Installing
+
+Use the Atom package manager and search for "linter-spell-html", or run
+`apm install linter-spell-html` from the command line.
+
+## Prerequisites
+
+This package relies upon [linter-spell](https://atom.io/packages/linter-spell)
+and it must therefore be installed.
+
+## Status
+
+Please note that this package is in an **alpha** state.
---
+++ spec/main-spec.js
@@ -0,0 +1,33 @@
+'use babel'
+
+import * as path from 'path'
+import * as _ from 'lodash'
+// import { Range } from 'atom'
+
+describe('The linter-spell-html provider for Atom Linter', () => {
+  const grammar = require('../lib/main').provideGrammar()[0]
+
+  beforeEach(() => {
+    waitsForPromise(() => {
+      return atom.packages.activatePackage('linter-spell-html')
+    })
+  })
+
+  // it('finds spelling regions in "foo.tex"', () => {
+  //   waitsForPromise(() => {
+  //     return atom.workspace.open(path.join(__dirname, 'files', 'foo.tex')).then(editor => {
+  //       expect(_.isEqual(
+  //         [new Range([2, 0], [3, 0]), new Range([4, 0], [0, 24])],
+  //         grammar.getRanges(editor, [editor.getBuffer().getRange()]).ignoredRanges)).toBe(true, 'Matching ranges')
+  //     })
+  //   })
+  // })
+
+  it('finds language "foo.html"', () => {
+    waitsForPromise(() => {
+      return atom.workspace.open(path.join(__dirname, 'files', 'foo.html')).then(editor => {
+        expect(_.isEqual(grammar.getDictionaries(editor), ['en_US'])).toBe(true, 'en_US dictionary')
+      })
+    })
+  })
+})
